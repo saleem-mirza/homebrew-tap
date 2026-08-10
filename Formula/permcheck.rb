4e1bb15207d05a866a8646d5904dcf89bc1da094
Saleem Mirza <saleem-mirza@users.noreply.github.com>
2026-08-10 00:17:07 +0000

    permcheck v0.1.21: update formula

--- Formula/permcheck.rb
+++ Formula/permcheck.rb
@@ -1,7 +1,7 @@
 class Permcheck < Formula
   desc "PreToolUse permission engine for Claude Code"
   homepage "https://github.com/saleem-mirza/marketplace"
-  version "0.1.20"
+  version "0.1.21"
   license "Apache-2.0"
 
   # Binaries are published as release assets on this public tap repo
@@ -9,11 +9,11 @@ class Permcheck < Formula
   on_macos do
     on_arm do
       url "https://github.com/saleem-mirza/homebrew-tap/releases/download/v#{version}/permcheck-darwin-arm64"
-      sha256 "29c263f32074e37b139dcc010f850e770b84bcb0cea0e000bfcd4f69161d9881"
+      sha256 "727396347d06de90ae0451a04adc1ca1bf469ab8f2d39cde5daf5846bf967a2b"
     end
     on_intel do
       url "https://github.com/saleem-mirza/homebrew-tap/releases/download/v#{version}/permcheck-darwin-x64"
-      sha256 "161297bf2372459beacfba5c84e8a428faf69ec0af374d21f01247b3c9505e94"
+      sha256 "6c1bd27ab51e0369350fb628ca064c820cece35fac699a3eb955f6dc6d50ffb7"
     end
   end
 
